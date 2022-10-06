54512a14cd0d26f9aae4a703fa1514e02f9c4840
Murttaz <vincentfournier1993@gmail.com>
2022-10-06 16:15:47 +0200

    init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,11 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour tout le monde");
+		
+	}
+
+}
